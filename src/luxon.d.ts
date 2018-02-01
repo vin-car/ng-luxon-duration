b9c10902efb3897f977c668ddd868a46c456a819
vin-car <v.carrese.dev@gmail.com>
2018-02-01 23:53:49 +0100

    Fix luxon.d.ts for fromMillis tests

--- src/luxon.d.ts
+++ src/luxon.d.ts
@@ -1,14 +1,16 @@
 declare module "luxon" {
   export class Duration {
 
-  	static fromISO(text: string, opts?: object): Duration;
-  	static fromMillis(count: number, opts?: object): Duration;
-  	static fromObject(obj: object): Duration;
+    static fromISO(text: string, opts?: object): Duration;
+    static fromMillis(count: number, opts?: object): Duration;
+    static fromObject(obj: object): Duration;
 
-  	as(unit: string): number;
-  	get(unit: string): number;
+    as(unit: string): number;
+    get(unit: string): number;
 
-  	toFormat(fmt: string, opts?: object): string;
-  	toObject(opts?: any): object;
+    toFormat(fmt: string, opts?: object): string;
+    toObject(opts?: any): object;
+
+    milliseconds: number;
   }
 }
