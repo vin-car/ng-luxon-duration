b827fdcfb4ca0f870d272b2fe025e22222efac9f
vin-car <v.carrese.dev@gmail.com>
2018-11-30 15:09:29 +0100

    Added tests on negative durations

--- src/duration-from-iso.pipe.test.ts
+++ src/duration-from-iso.pipe.test.ts
@@ -11,11 +11,11 @@ describe('DurationFromISOPipe', () => {
       durationFromISOPipe = new DurationFromISOPipe();
     });
 
-    test('durationFromISOPipe can parse a variety of ISO formats', () => {
-      const check = (s:string, ob:object) => {
-        expect(durationFromISOPipe.transform(s).toObject()).toEqual(ob);
-      };
+    const check = (s:string, ob:object) => {
+      expect(durationFromISOPipe.transform(s).toObject()).toEqual(ob);
+    };
 
+    test('durationFromISOPipe can parse a variety of ISO formats', () => {
       check('P5Y3M', { years: 5, months: 3 });
       check('PT54M32S', { minutes: 54, seconds: 32 });
       check('P3DT54M32S', { days: 3, minutes: 54, seconds: 32 });
@@ -23,11 +23,15 @@ describe('DurationFromISOPipe', () => {
       check('P2W', { weeks: 2 });
     });
 
-    test('Duration.fromISO can parse fractions of seconds', () => {
-      const check = (s:string, ob:object) => {
-        expect(durationFromISOPipe.transform(s).toObject()).toEqual(ob);
-      };
+    test("durationFromISOPipe can parse mixed or negative durations", () => {
+      check("P-5Y-3M", { years: -5, months: -3 });
+      check("PT-54M32S", { minutes: -54, seconds: 32 });
+      check("P-3DT54M-32S", { days: -3, minutes: 54, seconds: -32 });
+      check("P1YT-34000S", { years: 1, seconds: -34000 });
+      check("P-2W", { weeks: -2 });
+    });
 
+    test('Duration.fromISO can parse fractions of seconds', () => {
       check('PT54M32.5S', { minutes: 54, seconds: 32, milliseconds: 500 });
       check('PT54M32.53S', { minutes: 54, seconds: 32, milliseconds: 530 });
       check('PT54M32.534S', { minutes: 54, seconds: 32, milliseconds: 534 });
